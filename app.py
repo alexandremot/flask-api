66ab3b07a8454020273328d6990303f7539053de
Alexandre Mota <alexandremot@gmail.com>
2020-03-05 02:15:43 -0300

    adicionada funcao dados pedidos por mesa; ajustes menores no codigo

--- app.py
+++ app.py
@@ -2,24 +2,24 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
 import psycopg2
 
 
 # inicializa app
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-# database
+# inicializa database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/hotel'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# inicializa database
+
+# cria um objeto database
 db = SQLAlchemy(app)
-# inicializa marshmallow
+
+# cria um objeto marshmallow
 marshmallow = Marshmallow(app)
 
 
-# class/model pedido
+# definicão de class/model pedido
 class Pedido(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     mesa = db.Column(db.Integer)
@@ -34,7 +34,7 @@ class Pedido(db.Model):
         self.atendente = atendente
 
 
-# schema pedido
+# definição do schema pedido
 class PedidoSchema(marshmallow.Schema):
     class Meta:
         fields = ('id', 'mesa', 'pedido', 'quantidade', 'atendente')
@@ -44,7 +44,8 @@ class PedidoSchema(marshmallow.Schema):
 pedido_schema = PedidoSchema()
 pedidos_schema = PedidoSchema(many=True)
 
-# gera/inclui um pedido
+
+# [POST] gera um novo pedido
 @app.route('/create_pedido', methods=['POST'])
 def create_pedido():
     mesa = request.json['mesa']
@@ -58,7 +59,7 @@ def create_pedido():
     return pedido_schema.jsonify(novo_pedido)
 
 
-# atualiza/corrige pedido
+# [PUT] altera / atualiza / corrige um pedido pelo ID
 @app.route('/update_pedido/<id>', methods=['PUT'])
 def update_pedido(id):
     pedido = Pedido.query.get(id)
@@ -76,21 +77,32 @@ def update_pedido(id):
     db.session.commit()
     return pedido_schema.jsonify(pedido)
 
-# obtém a relação de todos os pedidos
-@app.route('/get_pedidos', methods=['GET'])
-def get_pedidos():
-    relacao_de_pedidos = Pedido.query.all()
-    resultado = pedidos_schema.dump(relacao_de_pedidos)
-    return jsonify(resultado)
 
-# obtém os dados de um pedido específico
+# [GET] obtém os dados de um pedido específico
 @app.route('/get_pedido/<id>', methods=['GET'])
 def get_pedido(id):
     dados_do_pedido = Pedido.query.get(id)
     resultado = pedido_schema.jsonify(dados_do_pedido)
     return resultado
 
-# deleta um pedido
+
+# [GET] obtem todos os dados dos pedidos realizados em uma mesa especifica
+@app.route('/get_pedidos_mesa/<num_mesa>', methods=['GET'])
+def get_pedidos_mesa(num_mesa):
+    pedidos_mesa = Pedido.query.filter_by(mesa=num_mesa).all()
+    resultado = pedidos_schema.dump(pedidos_mesa)
+    return jsonify(resultado)
+
+
+# [GET] obtem TODOS os dados de TODOS os pedidos
+@app.route('/get_pedidos', methods=['GET'])
+def get_pedidos():
+    relacao_de_pedidos = Pedido.query.all()
+    resultado = pedidos_schema.dump(relacao_de_pedidos)
+    return jsonify(resultado)
+
+
+# [DELETE] deleta um pedido especifico - pelo ID
 @app.route('/delete_pedido/<id>', methods=['DELETE'])
 def delete_pedido(id):
     pedido_a_deletar = Pedido.query.get(id)
